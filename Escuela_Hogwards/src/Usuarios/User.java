6e79efd41cf902d733a64ab5a925fbe5a0727097
ZxDrago <Usuario@laptop-casa>
2017-11-29 10:14:46 -0500

    Clase usuario lista

---
+++ Escuela_Hogwards/src/Usuarios/User.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Usuarios;
+
+/**
+ *
+ * @author Usuario
+ */
+public class User {
+    private String rol;
+    private String usuario,contraseña;
+
+    public User(String rol, String usuario, String contraseña) {
+        this.rol = rol;
+        this.usuario = usuario;
+        this.contraseña = contraseña;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+    
+    
+}
