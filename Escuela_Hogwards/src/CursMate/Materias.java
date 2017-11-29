de7d966fd05af138ef824912d7d5368e26833dd8
ZXDrago <henrymaticurena1997@gmail.com>
2017-11-29 11:27:08 -0500

    materias definidas

---
+++ Escuela_Hogwards/src/CursMate/Materias.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CursMate;
+
+/**
+ *
+ * @author Usuario
+ */
+public enum Materias {
+    Pociones, Defensascontraartesocuras,Adivinacion, Astronomia, Historiademagia, Herbologia,Encantamientos,Vuelo
+}
