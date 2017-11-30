0bbee685b3e67b7010ea8ebf1a511967970d4b87
ZXDrago <henrymaticurena1997@gmail.com>
2017-11-29 22:07:46 -0500

    menu principal de Estudiante hecho

--- Escuela_Hogwards/src/Usuarios/Estudiante.java
+++ Escuela_Hogwards/src/Usuarios/Estudiante.java
@@ -5,12 +5,36 @@
  */
 package Usuarios;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Usuario
  */
 public class Estudiante {
     
+    public void menuEstudiante(){
+        Scanner menE=new Scanner(System.in);
+        System.out.println("Escoja la opcion que desee hacer: ");
+        System.out.println("1. Consultar Curso");
+        System.out.println("2. Registro");
+        System.out.println("3. Descripcion de Vuelo");
+        int opcionEst=menE.nextInt();
+        switch(opcionEst){
+            case 1:
+                ConsultaCurso();
+                break;
+            case 2:
+                Registro();
+                break;
+            case 3:
+                DescripcionVuelo();
+                break;
+            default:
+                System.out.println("Usted ha finalizado sesion. ");
+        }
+
+    }
     public void ConsultaCurso(){}
     public void Registro(){}
     public void DescripcionVuelo(){}
