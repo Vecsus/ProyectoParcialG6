226c8e85689374d1a030b6bf911d1bee3876d04b
ZXDrago <henrymaticurena1997@gmail.com>
2017-12-02 01:00:50 -0500

    arreglos a clase animago

--- Escuela_Hogwards/src/Personas/Animagos.java
+++ Escuela_Hogwards/src/Personas/Animagos.java
@@ -12,10 +12,12 @@ package Personas;
 public class Animagos extends Brujo_Mago implements Volador{
     private String animal;
 
-    public Animagos(String animal, String nombre, String apellido, Casas_Hogwards casa, String varita, String cargo, int edad) {
-        super(nombre, apellido, casa, varita, cargo, edad);
+    public Animagos(String animal, String nombre, String apellido, String varita, String cargo, String fecha_ingreso, int edad, Casas_Hogwards casa) {
+        super(nombre, apellido, varita, cargo, fecha_ingreso, edad, casa);
         this.animal = animal;
     }
+
+
 
 
 
